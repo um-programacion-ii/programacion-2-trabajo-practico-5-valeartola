6407c1acf1ce35563832cbed8e5dbb2b08a63e30
valeartola <mv.artola@alumno.um.edu.ar>
2025-08-12 12:14:52 -0300

    implementación consultas personalizadas

--- sistema-gestion-empleados/src/main/java/com/example/sistema_gestion_empleados/repositories/EmpleadoRepository.java
+++ sistema-gestion-empleados/src/main/java/com/example/sistema_gestion_empleados/repositories/EmpleadoRepository.java
@@ -1,10 +1,27 @@
 package com.example.sistema_gestion_empleados.repositories;
 
+import com.example.sistema_gestion_empleados.entidades.Departamento;
 import com.example.sistema_gestion_empleados.entidades.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
+    Optional<Empleado> findByEmail(String email);
+    List<Empleado> findByDepartamento(Departamento departamento);
+    List<Empleado> findBySalarioBetween(BigDecimal salarioMin, BigDecimal salarioMax);
+    List<Empleado> findByFechaContratacionAfter(LocalDate fecha);
 
+    @Query("SELECT e FROM Empleado e WHERE e.departamento.nombre = :nombreDepartamento")
+    List<Empleado> findByNombreDepartamento(@Param("nombreDepartamento") String nombreDepartamento);
+
+    @Query("SELECT AVG(e.salario) FROM Empleado e WHERE e.departamento.id = :departamentoId")
+    Optional<BigDecimal> findAverageSalarioByDepartamento(@Param("departamentoId") Long departamentoId);
 }
